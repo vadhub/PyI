2dcba0efa0ea61e46ccd052c0f77b794026cf494
vadim <gabderahmanov99@gmail.com>
2026-03-16 00:20:13 +0300

    converter

--- app/src/main/java/com/abg/pyi/data/AppDatabase.kt
+++ app/src/main/java/com/abg/pyi/data/AppDatabase.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.abg.pyi.data.UserActivity
 import com.abg.pyi.data.UserActivityDao
 
 @Database(entities = [UserActivity::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userActivityDao(): UserActivityDao
 
