c51879c74732241b88de98b84e7bd33a752ea77a
vadim <gabderahmanov99@gmail.com>
2026-03-16 00:16:16 +0300

    calendar && architecture

---
+++ app/src/main/java/com/abg/pyi/data/Converters.kt
@@ -0,0 +1,19 @@
+package com.abg.pyi.data
+
+import androidx.room.TypeConverter
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class Converters {
+    @TypeConverter
+    fun fromLocalDate(date: LocalDate?): String? {
+        return date?.format(DateTimeFormatter.ISO_LOCAL_DATE)
+    }
+
+    @TypeConverter
+    fun toLocalDate(dateString: String?): LocalDate? {
+        return dateString?.let {
+            LocalDate.parse(it, DateTimeFormatter.ISO_LOCAL_DATE)
+        }
+    }
+}
